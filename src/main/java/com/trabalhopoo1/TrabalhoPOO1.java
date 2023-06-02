3d5ad97991e9c7e0617413ec43f1f92462c84545
gio-custodio <custodiogiovanna@gmail.com>
2023-06-02 17:05:38 -0300

    first commit

---
+++ src/main/java/com/trabalhopoo1/TrabalhoPOO1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.trabalhopoo1;
+
+/**
+ *
+ * @author custo
+ */
+public class TrabalhoPOO1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
